b9daa2fc013465c378052756b82205acbf9c47e5
Mohyibar <122816527+Mohyibar@users.noreply.github.com>
2025-09-09 19:19:41 +0300

    First Testing

--- Task1/server.py
+++ Task1/server.py
@@ -9,9 +9,14 @@ serverSocket.listen(1)
 print("The server is ready to receive")
 while True:
 	connectionSocket, addr = serverSocket.accept()
-	userInput = str(connectionSocket.recv(2048).decode())
-	if userInput and userInput[0] == '/':
-		userInput = userInput[1:]
+	userInput = str(connectionSocket.recv(1024).decode())
+	if not userInput or "filename=" not in userInput:
+		connectionSocket.close()
+		continue
+	print(userInput)
+	userInput = userInput.split(' ')[1][11:]
+	if userInput and userInput.startswith('%2F'):
+		userInput = userInput[3:]
 	print("IP: " + addr[0] + ", Port: " + str(addr[1]))
 	print("Requested resource: " + userInput)
 	if not os.path.exists(available_dir):
@@ -39,25 +44,26 @@ while True:
 					phrase = "200 OK"
 			else:
 				phrase = "404 Not Found"
-	print("Status code: " + phrase.split(" ")[0])
+	print("Status code: " + phrase.split(" ")[0] + "\n\n\n")
 	s=f"HTTP/1.1 {phrase}\r\n"
 	if phrase == "404 Not Found": userInput = "error404.html"
 	elif phrase == "403 Forbidden": userInput = "error403.html"
 	if phrase == "200 OK":
 		if flag:
 			s+='Content-Type: application/octet-stream\r\n'
-			#connectionSocket.send('Content-Disposition: attachment; filename="{}"\r\n'.format(userInput).encode())
-			f = open(os.path.join(available_dir, userInput), 'r')
+			s+=f'Content-Disposition: filename="{userInput}"\r\n'
+			f = open(os.path.join(available_dir, userInput), 'rb')
 			data = f.read()
 		else:
 			s+='Content-Type: text/html \r\n'
-			f = open(os.path.join(task1_dir, userInput), 'r')
+			f = open(os.path.join(task1_dir, userInput), 'rb')
 			data = f.read()
 	else:
 		s+='Content-Type: text/html \r\n'
 		f = open(os.path.join(task1_dir, userInput), 'r')
-		data = f.read().format(addr[0], addr[1])
-	s+='\r\n'+data
-	print(s)
+		data = f.read().format(addr[0], addr[1]).encode()
+	s+='\r\n'
+	# print(s)
 	connectionSocket.send(s.encode())
+	connectionSocket.send(data)
 	connectionSocket.close()
